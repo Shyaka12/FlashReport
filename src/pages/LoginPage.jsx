50f131cc63d3b0c33fd32142e21a6095ffc39dc3
shyaka12 <shyakap48@gmail.com>
2025-01-15 15:24:25 +0200

    Update dashboard design

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -122,7 +122,7 @@ const LoginPage = () => {
           <p className="text-sm text-gray-500 text-center mb-8">
             Don’t have an account?{" "}
             <a
-              href="/signup"
+              href="/src/pages/SignupPage.jsx"
               className="text-blue-600 font-medium hover:underline"
             >
               Create one here
